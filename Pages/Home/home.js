9db5d1bcedb6d8ca6f852ab78b08368bc9912529
ZeBolHD <zezebolz@gmail.com>
2022-10-21 11:57:12 +0300

    Initial commit

---
+++ Pages/Home/home.js
@@ -0,0 +1,36 @@
+const descriptions = {
+  desc1: document.getElementById("desc_1"),
+  desc2: document.getElementById("desc_2"),
+  desc3: document.getElementById("desc_3"),
+};
+
+let current_tab = 1;
+
+let next_btn = document.getElementById("next_btn");
+let prev_btn = document.getElementById("prev_btn");
+prev_btn.disabled = true;
+
+const changingTabs = (e) => {
+  e === "next" && current_tab++;
+  next_btn.disabled = current_tab === 3;
+
+  e === "prev" && current_tab--;
+  prev_btn.disabled = current_tab === 1;
+
+  descriptions.desc1.className =
+    current_tab === 1 ? "hero-description show" : "hero-description hide";
+  descriptions.desc2.className =
+    current_tab === 2 ? "hero-description show" : "hero-description hide";
+  descriptions.desc3.className =
+    current_tab === 3 ? "hero-description show" : "hero-description hide";
+
+  console.log(current_tab);
+};
+
+next_btn.addEventListener("click", () => {
+  changingTabs("next");
+});
+
+prev_btn.addEventListener("click", () => {
+  changingTabs("prev");
+});
